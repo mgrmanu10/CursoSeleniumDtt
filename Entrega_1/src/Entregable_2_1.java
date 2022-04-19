9a8b620ac581ed98b4e6996c4bf2fa310fe6052d
mgarciaromero <mgarciaromero@deloitte.es>
2022-04-19 21:26:59 +0200

    entrega 2 OK con scroll

--- Entrega_1/src/Entregable_2_1.java
+++ Entrega_1/src/Entregable_2_1.java
@@ -1,6 +1,7 @@
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
@@ -33,10 +34,14 @@ public class Entregable_2_1 {
         Thread.sleep(2000);  // Esperamos 2 segundos
 
 
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,3750)", "");
 
+        Thread.sleep(3000);
 
-        driver.findElement(By.xpath("/html/body/div[3]/div/div/article/div[3]/div[3]/div/table/tbody/tr[35]/td[4]/a")).click();
+        driver.findElement(By.xpath("/html/body/div[3]/div/div/article/div[3]/div[3]/div/table/tbody/tr[35]/td[4]/a")).click(); //Click en Ingeniería Informática
 
+        Thread.sleep(2000);
         //Cambio de Foco a la otra ventana
         Set<String> handles = driver.getWindowHandles();
         String[] ventanas_id = new String[2];
