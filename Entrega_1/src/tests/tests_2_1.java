52c2921a891e0c337dbce547e41df50d8911647a
mgarciaromero <mgarciaromero@deloitte.es>
2022-04-20 19:18:35 +0200

    Entrega 2 POM

---
+++ Entrega_1/src/tests/tests_2_1.java
@@ -0,0 +1,46 @@
+package tests;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import entregable_2_1.*;
+
+
+import java.lang.Thread;
+
+
+public class tests_2_1 {
+    public static void main (String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/mgarciaromero/Desktop/Manuel García Romero/SeleniumCurso/Entrega_1/DRIVERs/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String BaseUrl="https://www.uca.es/";
+        driver.get(BaseUrl);  // Accedemos a la URL de la UCA
+        driver.manage().window().maximize(); //maximizado de pantalla
+
+        Ventana ventana = new Ventana(driver);
+        Thread.sleep(2000);
+
+        ventana.clickCookies1();
+        ventana.clickEstudiantes();
+        Thread.sleep(1000);
+        ventana.clickGrados();
+
+        //Scroll
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,3750)", "");
+        Thread.sleep(1000);
+
+        ventana.clickIngenieriaInformatica();
+        ventana.newPage();
+        ventana.clickCookies2();
+        ventana.clickAcceso();
+        ventana.clickAcceso();
+        ventana.clickGradosbis();
+
+
+    }
+}
